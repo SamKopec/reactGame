d8dde498d8ca2f9d1ec2d18ddea25299e22a7254
sam kopec <skopec25@gmail.com>
2020-05-15 16:27:39 -0700

    typescript and prettier added, first board component connected

---
+++ src/components/Board.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Board extends Component {
+	render() {
+		return (
+			<div className="board">
+				<h1>Game Board</h1>
+			</div>
+		);
+	}
+}
+
+export default Board;
---
+++ src/types/data.ts
@@ -0,0 +1,12 @@
+export type PositionStatus = {
+	QB:  boolean,
+	RB:  boolean,
+	WR:  boolean,
+	TE:  boolean,
+	K:   boolean,
+	DEF: boolean
+}
+
+
+
+
